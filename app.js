10f02ce6781bc2d4f467d590d5c390ff90367ac4
wuweixian <wwx@thetiger.com.cn>
2019-07-29 09:49:19 +0800

    1

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('dist'));
+app.use(express.static('./dist'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
