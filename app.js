bd78f758f08b76bb23adbe9c2e027069f7aeb38e
wuweixian <wwx@thetiger.com.cn>
2019-07-29 14:39:19 +0800

    1

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
