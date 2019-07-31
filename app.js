13204a8355a8d17da9d030ab586726efa20f5549
wuweixian <wwx@thetiger.com.cn>
2019-07-31 18:37:22 +0800

    1

--- app.js
+++ app.js
@@ -38,4 +38,15 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+//拦截器允许跨域
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);  
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");  
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
+    res.header("X-Powered-By",' 3.2.1')  
+    res.header("Content-Type", "application/json;charset=utf-8");
+    res.header("Access-Control-Allow-Credentials", "true")
+    next();
+});
+
 module.exports = app;
--- routes/frontEnd/index.js
+++ routes/frontEnd/index.js
@@ -6,21 +6,32 @@ var httpRequest = require('../../config/httpRequest.js')
 /* GET home page. */
 router.get('/specialJob', function(req, res, next) {
 	//let data = await httpRequest('qzApi', 'GET', '/position/list', {count: 20, page: 1})
-	console.log(res.headersSent); // false
-  res.send('OK');
-  console.log(res.headersSent); // true
-	res.json('測試111');
-	// request({
-	// 		url: 'https://qiuzhi-api.lieduoduo.ziwork.com/position/list',
-	// 		method: 'GET',
-	// 		header: {
- //        'Authorization': '36b9e88fcb44bdb25602f67fbe35487b'
- //      }, 
-	// 		form: {count: 20, page: 1}
-	// 	}, function (err, response, body) {
-	// 		var data = JSON.parse(body)
-	// 		res.json(data);
-	// 	})
+	// console.log(JSON.parse(req.headers), 111)
+	let headers = {}
+	function titleCase(str) {
+	  let strArr = str.split(' ');
+	  for(let i=0;i<strArr.length;i++){
+	    strArr[i] = strArr[i].substring(0,1).toUpperCase()+strArr[i].toLowerCase().substring(1)
+	  } 
+	  return strArr.join(' ');
+	}  
+	if (req.headers) {
+		for (var i in req.headers) {
+			headers[`${titleCase(i)}`] = req.headers[i]
+		}
+	}
+	request({
+			url: 'https://qiuzhi-api.lieduoduo.ziwork.com/position/list',
+			method: 'GET',
+			headers: headers,
+			form: {count: 20, page: 1}
+		}, function (err, response, body) {
+			var data = JSON.parse(body)
+			res.json(data)
+			console.log(err, 111)
+			console.log(response, 222)
+			console.log(body, 333)
+		})
   
 });
 module.exports = router;
