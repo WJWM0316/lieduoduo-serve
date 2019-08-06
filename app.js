280d7d53155447b7e8880f2e09fcbb43a6131f18
wuweixian <wwx@thetiger.com.cn>
2019-08-06 09:55:34 +0800

    1

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/poster', canvasRoute);
+app.use('/frontEnd', canvasRoute);
 app.use('/frontEnd', frontEndRoute);
 app.use('/users', usersRouter);
 
--- routes/canvas/index.js
+++ routes/canvas/index.js
@@ -21,12 +21,24 @@ router.get('/rapidlyViwe', async function(req, res, next) {
 
 	const canvas = createCanvas(750, 1334);
 	const ctx = canvas.getContext('2d');
-	const bgSrc = path.join(__dirname, 'public/images/specialJobBg.png');
-	loadImage(bgSrc).then((image) => {
+	loadImage('https://pics5.baidu.com/feed/32fa828ba61ea8d39a420a0a54450d4b251f5834.jpeg?token=c005bc1a28f55f9caf030f47a8215d75&s=71331CD75E1327CC7F88BCAE03001009').then((image) => {
 		ctx.drawImage(image, 0, 0, 750, 1334);
-	})
-	canvas.toDataURL('image/jpeg', (err, jpeg) => { 
-		res.json(jpeg)
+		canvas.toDataURL('image/png', (err, jpeg) => {
+			let data = {
+				httpStatus: 200,
+				data: {
+					url: jpeg
+				}
+			}
+			// res.json(data)
+		 	res.render('index',{
+		 		 title:'study book' ,
+		 		 jpeg:jpeg,
+		 		 description:'照片墙'
+		 	})
+		})
+	}).catch(e => {
+		console.log(e, 3333333333333333)
 	})
 	
 })
