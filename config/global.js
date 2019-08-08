e0e8e5c54f9e8774e7afd819e3a75917bd1fb8d2
wuweixian <wwx@thetiger.com.cn>
2019-08-08 18:57:06 +0800

    1

--- config/global.js
+++ config/global.js
@@ -4,19 +4,19 @@ var nodeApi = "", // nodeJs 代理转发的域名
     zpApi   = "", // 招聘端的域名
     pubApi  = ""  // pub公共端域名
 switch (Node_Env) {
-    case 'test': // 测试
+    case 'dev': // 测试
         nodeApi = "https://node.lieduoduo.ziwork.com"
         qzApi   = "https://qiuzhi-api.lieduoduo.ziwork.com"
         zpApi   = "https://zhaopin-api.lieduoduo.ziwork.com"
         pubApi  = "https://pub-api.lieduoduo.ziwork.com"
         break;
-    case "preview": // 预发布
+    case "pre": // 预发布
         nodeApi = "https://node.lieduoduo.com"
         qzApi   = "https://qiuzhi-api.lieduoduo.com"
         zpApi   = "https://zhaopin-api.lieduoduo.com"
         pubApi  = "https://pub-api.lieduoduo.com"
         break
-    case "product": // 正式
+    case "pro": // 正式
         nodeApi = "https://node.lieduoduo.com"
         qzApi    = "https://qiuzhi-api.lieduoduo.com"
         zpApi   = "https://zhaopin-api.lieduoduo.com"
