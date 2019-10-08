9a1d7f0ad083ae1809293519fd7804186e876935
wuweixian <wwx@thetiger.com.cn>
2019-10-08 11:46:10 +0800

    1

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
 
 //拦截器允许跨域
 app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);  
+    res.header("Access-Control-Allow-Origin", "*");  
     res.header("Access-Control-Allow-Headers", "X-Requested-With");  
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");  
     res.header("X-Powered-By",' 3.2.1')  
