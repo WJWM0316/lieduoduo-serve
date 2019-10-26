299b5e1bbbc1856050a1a5e2e7b74fc1ba04abb2
wuweixian <wwx@thetiger.com.cn>
2019-10-26 16:44:23 +0800

    精美

--- routes/canvas/position_min.js
+++ routes/canvas/position_min.js
@@ -43,7 +43,7 @@ router.get('/position_min', async function(req, res, next) {
     hostType: 'pubApi', 
     method: 'GET', 
     url: `/share/position_share`, 
-    data: {positionId : req.query.id}, 
+    data: {positionId : req.query.id, type: qrpe}, 
     req,
     res,
     next
@@ -256,7 +256,7 @@ router.get('/position_min/1', async function(req, res, next) {
     hostType: 'pubApi', 
     method: 'GET', 
     url: `/share/position_share`, 
-    data: {positionId : req.query.id}, 
+    data: {positionId : req.query.id, type: qrpe}, 
     req,
     res,
     next
@@ -475,7 +475,7 @@ router.get('/position_min/2', async function(req, res, next) {
     hostType: 'pubApi', 
     method: 'GET', 
     url: `/share/position_share`, 
-    data: {positionId : req.query.id}, 
+    data: {positionId : req.query.id, type: qrpe}, 
     req,
     res,
     next
@@ -713,8 +713,8 @@ router.get('/position_min/3', async function(req, res, next) {
   let qrCode = await httpRequest({
     hostType: 'pubApi', 
     method: 'GET', 
-    url: `/share/position_share?type=qrpe`, 
-    data: {positionId : req.query.id}, 
+    url: `/share/position_share`, 
+    data: {positionId : req.query.id, type: qrpe}, 
     req,
     res,
     next
