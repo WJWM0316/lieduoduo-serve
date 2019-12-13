09d00920182b05099896f0d55f8c0bf2a29411f8
wuweixian <wwx@thetiger.com.cn>
2019-12-13 16:00:19 +0800

    1

--- app.js
+++ app.js
@@ -40,26 +40,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-//拦截器允许跨域
-app.all("*", function(req,res,next){
-    if(	 req.headers.origin.toLowerCase() === "http://www.lieduoduo.com"
-			|| req.headers.origin.toLowerCase() === "http://www.lieduoduo.ziwork.com"
-			|| req.headers.origin.toLowerCase() === "http://h5.lieduoduo.com"
-			|| req.headers.origin.toLowerCase() === "http://h5.lieduoduo.ziwork.com"
-			|| req.headers.origin.toLowerCase() === "http://m.lieduoduo.com"
-			|| req.headers.origin.toLowerCase() === "http://m.lieduoduo.ziwork.com"
-			) {
-			//设置允许跨域的域名，*代表允许任意域名跨域
-			res.header("Access-Control-Allow-Origin", req.headers.origin);
-		}
-    //允许的header类型
-    res.header("Access-Control-Allow-Headers","content-type");
-    //跨域允许的请求方式 
-    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-    if (req.method.toLowerCase() == 'options') {
-			res.send(200);  //让options尝试请求快速结束
-		} else {
-			next();
-		}
-})
 module.exports = app;
