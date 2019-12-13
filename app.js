d7a45d250816193065b637eed80469155d314f4e
wuweixian <wwx@thetiger.com.cn>
2019-12-13 15:51:44 +0800

    1

--- app.js
+++ app.js
@@ -56,9 +56,10 @@ app.all("*", function(req,res,next){
     res.header("Access-Control-Allow-Headers","content-type");
     //跨域允许的请求方式 
     res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
-    if (req.method.toLowerCase() == 'options')
-      res.send(200);  //让options尝试请求快速结束
-    else
-      next();
-}
+    if (req.method.toLowerCase() == 'options') {
+			res.send(200);  //让options尝试请求快速结束
+		} else {
+			next();
+		}
+})
 module.exports = app;
