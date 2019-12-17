c8872220222e5866a65f96e667a0cb1ab26bf6bd
wuweixian <wwx@thetiger.com.cn>
2019-12-17 10:49:28 +0800

    1

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var allowCors = function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
 	}
 	// res.header("Access-Control-Allow-Credentials", true);
-  // res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "*");
   // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   // res.header("X-Powered-By", 'Express');
   // res.header("Content-Type", 'application/x-www-form-urlencoded');
