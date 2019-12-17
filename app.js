0c0839f33b3c1ded6ba3510e03bafe33cee837b8
wuweixian <wwx@thetiger.com.cn>
2019-12-17 10:59:20 +0800

    1

--- app.js
+++ app.js
@@ -36,11 +36,11 @@ var allowCors = function(req, res, next) {
 	} else {
 		res.header("Access-Control-Allow-Origin", "*");
 	}
-	// res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Headers", "*");
-  // res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  // res.header("X-Powered-By", 'Express');
-  // res.header("Content-Type", 'application/x-www-form-urlencoded');
+	res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, X-Requested-With, Authorization, Wechat-Version, Admin-Version, Authorization-Admin, Authorization-Wechat, Authorization-Official, Channel-Code, Channel-Url, Act-Code, Act-Pid, Location, Notify-Appid, Authorization-App, Authorization-App-Wechat, App, App-Model, App-Version, Device-Id, App-Channel, Msg-Id, Source, Idfa-IOS, Exquisite-Code");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", 'Express');
+  res.header("Content-Type", 'application/x-www-form-urlencoded');
   console.log(new Date())
   next()
 };
