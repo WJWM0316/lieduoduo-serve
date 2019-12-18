904687a18c84f29ac1d5c896cc86bcfbdac9f6cb
wuweixian <wwx@thetiger.com.cn>
2019-12-18 17:52:08 +0800

    要你节支持

--- routes/canvas/wantYou.js
+++ routes/canvas/wantYou.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
-// var QRCode = require('qrcode');
+var fs = require('fs');
+
 var public = path.resolve('./public')
 var {createCanvas, loadImage, registerFont} = require('canvas');
 var Global = require("../../config/global.js"); //根据环境变量，获取对应的IP
+var myUpload = require("../../api/myUpload.js");
 
 registerFont(public + '/font/PingFangSC.ttf', { family: 'PingFangSC' })
 registerFont(public + '/font/PingFangSC-bold.ttf', { family: 'PingFangSC-bold' })
@@ -12,10 +14,10 @@ registerFont(public + '/font/PingFangSC-light.ttf', { family: 'PingFangSC-light'
 
 var httpRequest = require('../../config/httpRequest.js')
 var pocessor = require('../../utils/canvasPocessor.js')
-router.get('/test', function(req, res, next) {
-  res.json(111)
-})
-router.get('/wantYou', async function(req, res, next) {
+
+
+
+router.get('/wantActivityPoster', async function(req, res, next) {
   const canvas = createCanvas(750, 1334);
   const ctx = canvas.getContext('2d');
   ctx.textBaseline = "top"
@@ -24,8 +26,7 @@ router.get('/wantYou', async function(req, res, next) {
     req.headers['Authorization'] = req.headers['authorization-app']
   }
   // 画背景
-  
-  let imgUrl = await loadImage(public + '/images/wantYouBg.jpg')
+  let imgUrl = req.query.type === 'activity3' ? await loadImage(public + '/images/wantYou_activity3.jpg') : await loadImage(public + '/images/wantYou_activity2.jpg')
   ctx.drawImage(imgUrl, 0, 0, 750, 1334);
 
   let data = await httpRequest({
@@ -38,54 +39,16 @@ router.get('/wantYou', async function(req, res, next) {
     next
   })
   let info = data.data || null
-  
-  if (!info.id) {
-    let data2 = await httpRequest({
-      hostType: 'zpApi', 
-      method: 'GET', 
-      url: `/cur/user_info`, 
-      data: req.query, 
-      req,
-      res,
-      next
-    })
-    info.uid = data2.data.id
-    info.avatar = {
-      smallUrl: data2.data.avatarInfo.smallUrl
-    }
-    let data3 = await httpRequest({
-      hostType: 'zpApi', 
-      method: 'GET', 
-      url: `/company/identity`, 
-      data: req.query, 
-      req,
-      res,
-      next
-    })
-    info.position = data3.data.companyInfo.userPosition
-    info.name = data3.data.companyInfo.realName
-    info.companyShortname = data3.data.companyInfo.companyShortname || data3.data.companyInfo.companyName
-  }
+
   // 头像
   ctx.save();
-  ctx.arc(298 + 78, 407 + 78, 78, 0, Math.PI * 2);
+  ctx.arc(287 + 87, 184 + 87, 87, 0, Math.PI * 2);
   
   ctx.clip();
   let avatarUrl = await loadImage(info.avatar.smallUrl)
-  ctx.drawImage(avatarUrl, 298, 407, 156, 156)
+  ctx.drawImage(avatarUrl, 287, 184, 174, 174)
   ctx.restore()
 
-  ctx.fillStyle = '#ffffff';
-  
-  ctx.textAlign = 'center';
-  ctx.font = 'bold 46px PingFangSC-bold'
-  ctx.fillText(info.companyShortname, 375, 141)
-  ctx.font = '28px PingFangSC';
-  ctx.fillText('“ 招聘路上太孤单，不想再孤身奋战 ”', 375, 760)
-  ctx.font = 'bold 40px PingFangSC-bold'
-  ctx.fillText(info.name, 375, 598)
-  ctx.font = '24px PingFangSC';
-  ctx.fillText(info.position, 375, 652)
 
   let p = `${Global.webHost}/wantYou_b?vkey=sdfcxfe&uid=${req.query.uid}`
   let qrCodeData = await httpRequest({
@@ -97,29 +60,38 @@ router.get('/wantYou', async function(req, res, next) {
     res,
     next
   })
-  ctx.arc(495 + 92, 1004 + 92,  92, 0, Math.PI * 2);
+  ctx.arc(270 + 105, 1007 + 105,  105, 0, Math.PI * 2);
   ctx.clip();
   if (qrCodeData.data.url) {
     let qrCode = await loadImage(qrCodeData.data.url)
-    ctx.drawImage(qrCode, 495, 1004, 185, 185);
+    ctx.drawImage(qrCode, 270, 1007, 210, 210);
   }
   
   
 
   canvas.toDataURL('image/png', (err, jpeg) => {
-    let data = {
-      httpStatus: 200,
-      data: {
-        url: jpeg,
-        detail: info
-      }
-    }
-    res.json(data)
-    // res.render('index',{
-    //     title:'study book' ,
-    //     jpeg:jpeg,
-    //     description:'照片墙'
-    //  })
+    var base64Data = jpeg.replace(/^data:image\/\w+;base64,/, "");
+    let dataBuffer = Buffer.from(base64Data, 'base64');
+    let path = `${public}/files/${req.query.uid}.jpg`,
+       fileName = `${req.query.uid}_${new Date().getTime()}.jpg`
+    fs.writeFileSync(path, dataBuffer)
+    myUpload({fileName, files: path}).then(res0 => {
+     let jsonData = {
+       httpStatus: 200,
+       data: {
+         url: `${Global.cdnHost}/${res0.name}`
+       }
+     }
+     res.json(jsonData)
+    }).catch(e => {
+     res.json(e)
+     console.log(e, 33)
+    })
+      // res.render('index',{
+   //      title:'study book' ,
+   //      jpeg:jpeg,
+   //      description:'照片墙'
+   //   })
   });
 })
 
