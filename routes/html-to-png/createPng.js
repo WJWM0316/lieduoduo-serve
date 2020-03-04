cb25f108608c0306ae88d5fb95194d8711dfd82e
archer <amaohe@thetiger.com.cn>
2020-03-04 16:17:11 +0800

    feat htmltopng

--- routes/html-to-png/createPng.js
+++ routes/html-to-png/createPng.js
@@ -14,16 +14,15 @@ router.get('/render', async(req, res, next) => {
 
 router.get('/pngs', async(req, res, next) => {
     // http://127.0.0.1:3100/json/version
-    // let version = await request({
-    //     uri:  "http://127.0.0.1:9222/json/version",
-    //     json: true
-    // });
-    // version.webSocketDebuggerUrl
+    let version = await request({
+        uri:  "http://127.0.0.1:3100/json/version",
+        json: true
+    });
     let browser = await puppeteer.connect({
         ignoreHTTPSErrors: true,
-        browserWSEndpoint: 'ws://127.0.0.1:3100'
+        browserWSEndpoint: version.webSocketDebuggerUrl
     });
-    const page = await browser.newPage();
+    /* const page = await browser.newPage();
     // 模拟一个 iPhone X
     await page.emulate(deviceModel);
     // await page.setViewport({ width: 375, height: 812 });
@@ -32,12 +31,13 @@ router.get('/pngs', async(req, res, next) => {
         type: 'png',
         encoding: 'base64',
     });
-    await page.close();
-    res.render('index', {
-        title:'study book',
-        jpeg:`data:image/png;base64,${results}` ,
-        description:'照片墙'
-    })
+    await page.close(); */
+    res.json(version)
+    // res.render('index', {
+    //     title:'study book',
+    //     jpeg:`data:image/png;base64,${results}` ,
+    //     description:'照片墙'
+    // })
 })
 
 module.exports = router;
