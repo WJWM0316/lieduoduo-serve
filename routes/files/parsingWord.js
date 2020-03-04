222c0b87e72b9638c202dced7cf01c3c1d91dc2a
wuweixian <wwx@thetiger.com.cn>
2020-03-04 11:24:52 +0800

    1

--- routes/files/parsingWord.js
+++ routes/files/parsingWord.js
@@ -19,7 +19,7 @@ function parseWord(filePath, res) {
       break
     case 'pdf':
       config.preserveLineBreaks = true
-      config.layout = 'raw'
+      
       break
   }
   textract.fromFileWithPath(filePath, config, function (error, text) {
