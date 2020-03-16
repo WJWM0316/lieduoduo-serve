cfa992578508dc3f8a47d2b1ab0b0afd5e8e8623
wuweixian <wwx@thetiger.com.cn>
2020-03-16 14:50:14 +0800

    1

--- routes/html-to-png/render.js
+++ routes/html-to-png/render.js
@@ -39,7 +39,7 @@ router.get('/s-position', async(req, res, next) => {
     if (req.headers['authorization-app']) {
       req.headers['Authorization'] = req.headers['authorization-app']
     }
-    console.log(req, 111)
+    console.log(req.headers, 111)
     const {id} = req.query
     const getDatas = httpRequest({ hostType: 'qzApi', method: 'GET',  url: `/position/${id}`, data: req.query, req, res, next })
     const getQrCode = httpRequest({ hostType: 'pubApi', method: 'GET', url: `/share/position_share`, data: {positionId : id, type: 'qrpl'}, req,res, next })
